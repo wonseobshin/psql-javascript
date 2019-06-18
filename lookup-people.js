506c9fcc2c40578a07136792aef50b59edf845ba
Won Seob Shin <wonseob07@gmail.com>
2019-06-18 20:24:19 +0000

    added script named lookup-people.js to look up a persons firstname using argument from node

---
+++ lookup-people.js
@@ -0,0 +1,30 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const args = process.argv.slice(2);
+console.log(args);
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+
+  console.log("successfully connected");
+
+  client.query(`SELECT * FROM famous_people WHERE first_name LIKE $1`, args, (err, res) => {
+    if(err) throw err;
+    res.rows.forEach((element) => {
+      const simpleDOBArr = element.birthdate.toString().split(' ');
+      const simpleDOB = `${simpleDOBArr[3]}-${simpleDOBArr[1]}-${simpleDOBArr[2]}`
+      console.log(`- ${element.id}: ${element.first_name} ${element.last_name}, born '${simpleDOB}'`);
+    })
+    client.end();
+  })
+});
